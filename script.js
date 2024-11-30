3dbc6f55e1253e40c31bf7b3cb37376e7b8097b0
Adriano <adriano.limeira@academico.ifpb.edu.br>
2024-11-30 19:31:06 -0300

    Atualização dos arquivos

--- script.js
+++ script.js
@@ -1,21 +1,16 @@
-// Seleção dos elementos
 const inputText = document.getElementById('input-text');
 const addBtn = document.getElementById('add-btn');
 const paragraphContainer = document.getElementById('paragraph-container');
 const countBtn = document.getElementById('count-btn');
 
-// Adiciona um parágrafo
 addBtn.addEventListener('click', () => {
   const text = inputText.value.trim();
 
-  // Validação para não adicionar entradas vazias
   if (text !== '') {
-    // Criação do parágrafo
     const paragraph = document.createElement('p');
     paragraph.textContent = text;
     paragraphContainer.appendChild(paragraph);
 
-    // Limpa o input
     inputText.value = '';
     inputText.focus();
   } else {
@@ -23,7 +18,6 @@ addBtn.addEventListener('click', () => {
   }
 });
 
-// Mostra a quantidade de parágrafos
 countBtn.addEventListener('click', () => {
   const totalParagraphs = paragraphContainer.querySelectorAll('p').length;
   const message = `${totalParagraphs} linha${totalParagraphs !== 1 ? 's' : ''} foram adicionada${totalParagraphs !== 1 ? 's' : ''}.`;
